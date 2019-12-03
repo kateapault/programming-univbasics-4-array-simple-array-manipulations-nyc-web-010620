388d08fee4ac2989efd819d4e661775cc031bb65
Kate Raskauskas <katherine.raskauskas@gmail.com>
2019-12-03 02:15:56 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,19 +10,22 @@ def using_pop(array)
   array.pop
 end
 
-def pop_with_args(array,num)
-  array.pop(num)
+def pop_with_args(array)
+  array.pop(2)
 end
 
 def using_shift
   array.shift
 end
 
-def shift_with_args(array,num)
-  array.shift(num)
+def shift_with_args(array)
+  array.shift(2)
 end
 
 def using_concat(array1,array2)
   array1.concat(array2)
 end
 
+def using_insert(array,element)
+  array.insert(element,2)
+end
