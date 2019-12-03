241cedb9a64fa5a658fba953608c19ce134108ab
Kate Raskauskas <katherine.raskauskas@gmail.com>
2019-12-03 02:14:39 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,24 @@
+def using_push(array,string)
+  array.push(string)
+end
+
+def using_unshift(array,string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array,num)
+  array.pop(num)
+end
+
+def using_shift
+  array.shift
+end
+
+def shift_with_args(array,num)
+  array.shift(num)
+end
+
